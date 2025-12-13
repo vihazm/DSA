6990113c680cfbc7cdd356ad46112eb4728ea24a
vihazm <vihassanchith5@gmail.com>
2025-12-13 14:45:14 +0530

    Add files via upload

---
+++ QueueStackPushFriendly.java
@@ -0,0 +1,63 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dsa;
+import java.util.LinkedList;
+import java.util.Queue;
+/**
+ *
+ * @author Vihas
+ */
+public class QueueStackPushFriendly {
+    
+    Queue<Integer> q1 = new LinkedList<>();
+    Queue<Integer> q2 = new LinkedList<>();
+
+    // PUSH operation (O(1))
+    public void push(int data) {
+        q1.add(data);
+        System.out.println("Pushed: " + data);
+    }
+
+    // POP operation (O(n))
+    public int pop() {
+        if (q1.isEmpty()) {
+            System.out.println("Stack is empty!");
+            return -1;
+        }
+
+        // Move elements except last to q2
+        while (q1.size() > 1) {
+            q2.add(q1.remove());
+        }
+
+        // Last element is the stack top
+        int popped = q1.remove();
+
+        // Swap q1 and q2
+        Queue<Integer> temp = q1;
+        q1 = q2;
+        q2 = temp;
+
+        return popped;
+    }
+
+    // DISPLAY stack
+    public void display() {
+        System.out.println("Current Stack (top at right): " + q1);
+    }
+
+    public static void main(String[] args) {
+        QueueStackPushFriendly stack = new QueueStackPushFriendly();
+
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+
+        stack.display();
+
+        System.out.println("Popped: " + stack.pop());
+        stack.display();
+    }
+}
